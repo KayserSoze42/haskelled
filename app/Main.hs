9f46e7694186833696509258cb6b04ec9a66d6bd
KayserSoze42 <morus.hana@gmail.com>
2023-09-07 12:44:22 +0200

    rjsonified
    show, don't tell
    
    plz

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,7 @@ module Main where
 import Data.Time.Clock (getCurrentTime, diffUTCTime)
 
 import qualified M03
-                   ( JAAASON (..) 
+                   ( JAAASON (..)  
 		   )
 main :: IO ()
 main = do
@@ -12,51 +12,7 @@ main = do
 
   -- dumb stuff go here --
 
-  let jSON0 = M03.JSTR "if it compiles it works, right? .. RIGHT?!" :: M03.JAAASON
-  let jSON1 = M03.JNMR 69 :: M03.JAAASON
-  let jSON2 = M03.JBOO True :: M03.JAAASON
-
-  let jSOF0 = M03.JOBB [("bigiftrue", jSON0)] :: M03.JAAASON
-  let jSOF1 = M03.JOBB [("niceiftrue", jSON1)] :: M03.JAAASON
-  let jSOF2 = M03.JOBB [("trueiftrue", jSON2)] :: M03.JAAASON
-
-  let jARR0 = M03.JARR [jSON0, jSON1] :: M03.JAAASON
-  let jARR1 = M03.JARR [jSON1, jSON2] :: M03.JAAASON
-  let jARR2 = M03.JARR [jSON0, jSON2] :: M03.JAAASON
-
-  let wARR0 = M03.JARR [jSOF0, jSOF2] :: M03.JAAASON
-  let wARR1 = M03.JARR [jSOF1, jSON0] :: M03.JAAASON
-  let wARR420 = M03.JARR [wARR0, jARR1] :: M03.JAAASON -- aka, make jar not war < 3
-
-  putStrLn ("the jsons: ")
-
-  putStrLn (show jSON0) 
-  putStrLn (show jSON1) 
-  putStrLn (show jSON2)
-
-  putStrLn ("\n--\n")
-
-  putStrLn (show jSOF0)
-  putStrLn (show jSOF1)
-  putStrLn (show jSOF2)
- 
-  putStrLn ("\n--\n")
   
-  putStrLn (show jARR0)
-  putStrLn (show jARR1)
-  putStrLn (show jARR2)
- 
-  putStrLn ("\n--\n")
-  
-  putStrLn (show wARR0)
-  putStrLn (show wARR1)
-  putStrLn (show wARR420)
-
-  putStrLn ("\n-#-#-#-#-#-#-")
-
-  putStrLn ("\nand the go-getters: ")
-
-  putStrLn ("jSON!: " ++ show (M03.jSTR jSON0))
 
   executionEnd <- getCurrentTime
   
--- src/M0xs/M03.hs
+++ src/M0xs/M03.hs
@@ -2,11 +2,30 @@ module M03
          ( JAAASON (..)
 	 ) where
 
+import Data.Char 
+               ( ord
+	       , chr
+	       , isHexDigit
+	       , isDigit
+	       , isSpace
+	       , digitToInt
+	       ) 
+
+import Data.List 
+               ( intercalate
+	       )
+
+import Numeric 
+             ( showHex
+	     )
+
 data JAAASON = JSTR {
                       jSTR :: [Char]
 		    }
-             | JNMR { 
-	              jNMR :: Int
+             | JNMR 
+	            { jINT :: Integer
+		    , jFRC :: [Int]
+		    , jEXP :: Integer
 		    }
 	     | JBOO {
 	              jBOO :: Bool
@@ -18,6 +37,48 @@ data JAAASON = JSTR {
 	     | JARR {
 	              jARR :: [JAAASON]
 		    }
-	     deriving (Eq, Ord, Show)
+	     deriving (Eq, Ord)
 
+instance Show JAAASON where
+  show jaaason = case jaaason of
+                                JNOO              -> "nun_here"
+			        JBOO True         -> "true"
+			        JBOO False        -> "false"
+			        JSTR txt          -> showMeTheJSoup txt
+			        JNMR txt [] 0     -> show txt
+			        JNMR txt foo 0    -> show txt ++ "." ++ concatMap show foo
+			        JNMR txt [] elmo  -> show txt ++ "e" ++ show elmo
+			        JNMR txt foo elmo -> show txt ++ "." ++ concatMap show foo ++ "e" ++ show elmo
+			        JARR arr          -> "[" ++ intercalate ", " (map show arr) ++ "]"
+			        JOBB obb          -> "{" ++ intercalate ", " (map showKeyVals obb) ++ "}"
+			                                                                                 where showKeyVals (key, val) = showMeTheJSoup key ++ ": " ++ show val
+
+
+showMeTheJSoup :: [Char] -> [Char]
+showMeTheJSoup txt = "\""
+
+showMeTheJSauce :: Char -> [Char]
+showMeTheJSauce chr = case chr of 
+                                  '\''          -> "'"
+				  '\"'          -> "\\\"" -- hmmm
+				  '\\'          -> "\\\\" 
+				  '/'           -> "\\/"  -- ... hmmmm ...
+				  '\b'          -> "\\b"
+				  '\f'          -> "\\f"  --     .....
+				  '\n'          -> "\\n"
+				  '\r'          -> "\\r"  -- ...       ...
+				  '\t'          -> "\\t"
+				  _
+				   | isCTRL chr -> "\\u" ++ showMeTheCee chr
+				   where showMeTheCee chr = 
+				                            let ah = "0000" ++ showHex (ord chr) "" in
+							                                               drop (length ah - 4) ah -- ... ... *nodd* 
+				  _             -> [chr]
+						                                               
+												                                                  -- *nodd* ... ...
+												                               -- hmh hmh ... ...
+															                          -- ... ... mhm mhm
+
+isCTRL :: Char -> Bool
+isCTRL chr = chr `elem` ['\0' .. '\31']
 
